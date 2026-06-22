becb4d6fc25ee8791706c42fb5eff39c58711a5f
Eden Fox Phillips <foxhopper16@gmail.com>
2026-06-22 23:03:53 +0100

    adjust section

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
       <HeroSection />
       <OurRootsSection />
       <ScriptureSection />
-      <SevenSorrowsSection />
       <CharismSection />
+      <SevenSorrowsSection />
       <LifeInTheOrderSection />
       <PatronSaintsSection />
       <EventsPreviewSection />
